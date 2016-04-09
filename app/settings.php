561fa562991da5d5d0210a33d4cdb4c0f5f453bd
Janis <janis.angst@gmail.com>
2016-04-09 14:51:35 +0200

    Added content to the order-, settings- and userpage.

--- app/settings.php
+++ app/settings.php
@@ -6,3 +6,148 @@
  * Time: 15:37
  */
 ?>
+
+<div id="content" style="padding-left:10px; padding-right:10px;"><div class="row">
+	<div id="" class="col-sm-3">
+	    <label class="col-sm-3 control-label">Shopname</label>
+	    <div class="col-sm-9">
+	        <input type="text" class="form-control" name="title" placeholder="Webshop Name" value="Mein Bauernhof">
+	    </div>
+	    <label class="col-sm-3 control-label">Name</label>
+	    <div class="col-sm-9">
+        	<input type="text" class="form-control" name="name" placeholder="Max Mustermann" value="Max Mustermann">
+        </div>
+        <label class="col-sm-3 control-label">Strasse</label>
+        <div class="col-sm-9">
+            <input type="text" class="form-control" name="street" placeholder="Musterstrasse 1" value="Musterstrasse 1">
+        </div>
+        <label class="col-sm-3 control-label">PLZ</label>
+        <div class="col-sm-9">
+            <input type="text" class="form-control" name="plz" placeholder="1234" value="1234" onkeydown="return isNumberKey(event)">
+        </div>
+        <label class="col-sm-3 control-label">Ort</label>
+        <div class="col-sm-9">
+            <input type="text" class="form-control" name="city" placeholder="Musteren" value="Musteren">
+        </div>
+	</div>
+
+	<div class="form-group" class="col-sm-7">
+	    <label class="col-sm-12 control-label">Über Uns</label>
+	    <div class="col-sm-12">
+	        <textarea rows="5" class="form-control editme" name="aboutUs" id="aboutUs" placeholder="Über Uns">
+	        </textarea>
+	    </div>
+	    <label class="col-sm-12 control-label">Öffnungszeiten</label>
+	    <div class="col-sm-12">
+	        <textarea rows="3" class="form-control editme" name="opening" id="opening" placeholder="Öffnungszeiten">
+	        </textarea>
+	    </div>
+	</div>
+
+	<div id="" class="col-sm-2">
+        <label class="col-sm-12 control-label">Logo</label>
+        <div class="col-sm-12">
+            <input type="text" class="form-control" name="logo" placeholder="" value="">
+        </div>
+        <div class="col-sm-4 col-sm-offset-3">
+            <button type="file" class="btn" name="logo" accept="image">Durchsuchen...</button>
+        </div>
+        <label class="col-sm-12 control-label">Farbe</label>
+        <div class="col-sm-12">
+            <form>
+                <input type="radio" name="color" value="red" checked>Rot</input>
+                <input type="radio" name="color" value="blue">Blau</input><br />
+                <input type="radio" name="color" value="green">Grün</input>
+                <input type="radio" name="color" value="yellow">Gelb</input><br />
+                <input type="radio" name="color" value="gray">Grau</input>
+            </form>
+        </div>
+	</div>
+
+<script type="text/javascript">
+
+function isNumberKey(evt){
+    var charCode = (evt.which) ? evt.which : event.keyCode
+    if (charCode > 31 && (charCode < 48 || charCode > 57))
+        return false;
+    return true;
+}
+
+
+CKEDITOR.config.toolbar = [
+   ['Styles','Format','Font','FontSize'],
+   ['Bold','Italic','Underline','StrikeThrough','-','-','Cut','Copy','Paste','Find','Replace','-','Outdent','Indent','-','Print'],
+   ['NumberedList','BulletedList','-','JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
+   ['Table','-','Link','TextColor','BGColor','Source']
+] ;
+
+
+var descriptionEditor = CKEDITOR.replace( 'description' );
+Dropzone.autoDiscover = false;
+
+var dz = $('#picture').dropzone({
+    url: "upload.php",
+    clickable:'#clickZone',
+    createImageThumbnails: true,
+    acceptedFiles: 'image/*',
+    addRemoveLinks: false,
+    thumbnailWidth: "120",
+    thumbnailHeight: "120",
+    maxFiles: 1,
+
+    init: function() {
+
+   	  this.on('error', function(file) {
+   	  	Dropzone.forElement('#picture').removeFile(file);
+   	  	if(!isImage(file.name)){
+        	$.notify("Falscher Datentyp", "error");
+		}
+      });
+
+      this.on('maxfilesexceeded', function(file) {
+        Dropzone.forElement('#picture').removeAllFiles(true);
+        this.addFile(file);
+      });
+
+      this.on('success', function(file, json) {
+        document.getElementById("logo").value = json;
+      });
+    }
+  });
+
+  Dropzone.forElement('#picture').removeAllFiles(true);
+  var mockFile = { name: "Logo", size: 12345, status: 'success', accepted: true, bytesSent: 12345};
+
+  Dropzone.forElement('#picture').emit("addedfile", mockFile);
+  Dropzone.forElement('#picture').emit("thumbnail", mockFile, 'http://linux203.cs.technik.fhnw.ch/magento/skin/frontend/rwd/default/images/logo.gif?time=' + Math.random().toString(36).substr(2, 5));
+  Dropzone.forElement('#picture').files.push( mockFile );
+  Dropzone.forElement("#picture").emit("complete", mockFile);
+
+function updateSetting(){
+
+document.getElementById('description').value = descriptionEditor.getData();
+
+ var payload = JSON.stringify($('form').serializeObject());
+
+$.ajax({
+        url : 'rest/put_setting/',
+        type: 'POST',
+        data: payload,
+        success: function(data){
+            $.notify("Gespeichert", "success");
+        },
+        error: function(){
+        	$.notify("Error", "error");
+        }
+    });
+
+};
+
+    $(function(){
+        $('.picker').colorpicker().on('changeColor.colorpicker', function(event){
+        document.getElementById('color').value = event.color.toHex();
+      });
+    });
+
+</script>
+</div>
---
+++ app/users.php
@@ -0,0 +1,144 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Janis
+ * Date: 08.04.2016
+ * Time: 20:44
+ */
+
+session_start();
+
+if(!isset($_SESSION['username'])) {
+ return header('Location: index.php');
+}
+?>
+
+<div id="content" style="padding-left:50px; padding-right:50px;">
+        <script type="text/javascript">
+            loadItem('users', 'content_table', 1);
+        </script>
+
+        <br><br>
+        <div id="content_table">
+            <div class="table-responsive rwd-article">
+                <div id="data-table_wrapper" class="dataTables_wrapper form-inline dt-bootstrap no-footer">
+                    <div class="row">
+                        <div class="col-sm-6"></div>
+                        <div class="col-sm-6">
+                            <div id="data-table_filter" class="dataTables_filter">
+                                <label>Suchen<input type="search" class="form-control input-sm" placeholder="" aria-controls="data-table"></label>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <table class="table table-hover table-striped table-bordered dataTable no-footer" id="data-table" style="width: 100%;" role="grid" aria-describedby="data-table_info">
+                                <thead class="tablebold">
+                                <tr role="row">
+                                    <td class="sorting_asc" aria-controls="data-table" rowspan="1" colspan="1" aria-sort="ascending" aria-label="ID: aktivieren, um Spalte absteigend zu sortieren" style="width: 136px;"><div style="width: 48px;">Vorname</div></td>
+                                    <td class="sorting" aria-controls="data-table" rowspan="1" colspan="1" aria-label="Titel: aktivieren, um Spalte aufsteigend zu sortieren" style="width: 242px;">Nachname</td>
+                                    <td class="sorting" aria-controls="data-table" rowspan="1" colspan="1" aria-label="Titel: aktivieren, um Spalte aufsteigend zu sortieren" style="width: 242px;">Adresse</td>
+                                    <td class="sorting" aria-controls="data-table" rowspan="1" colspan="1" aria-label="Titel: aktivieren, um Spalte aufsteigend zu sortieren" style="width: 242px;">PLZ</td>
+                                    <td class="sorting" aria-controls="data-table" rowspan="1" colspan="1" aria-label="Titel: aktivieren, um Spalte aufsteigend zu sortieren" style="width: 242px;">Ort</td>
+                                    <td class="sorting" aria-controls="data-table" rowspan="1" colspan="1" aria-label="Titel: aktivieren, um Spalte aufsteigend zu sortieren" style="width: 242px;">Telefonnr.</td>
+                                    <td class="sorting" aria-controls="data-table" rowspan="1" colspan="1" aria-label="Titel: aktivieren, um Spalte aufsteigend zu sortieren" style="width: 242px;">E-Mail</td>
+                                    <td class="sorting" aria-controls="data-table" rowspan="1" colspan="1" aria-label="Titel: aktivieren, um Spalte aufsteigend zu sortieren" style="width: 242px;">Geburtstagsdatum</td>
+                                    <td class="sorting" aria-controls="data-table" rowspan="1" colspan="1" aria-label="Titel: aktivieren, um Spalte aufsteigend zu sortieren" style="width: 242px;">Newsletter</td>
+                                    <td class="sorting" aria-controls="data-table" rowspan="1" colspan="1" aria-label="Titel: aktivieren, um Spalte aufsteigend zu sortieren" style="width: 242px;">Einstellung</td>
+                                </tr>
+                                </thead>
+
+                                <tbody>
+                                <tr role="row" class="odd">
+                                    <td class="sorting_1">Max</td>
+                                    <td>Mustermann</td>
+                                    <td>Musterstrasse 14</td>
+                                    <td>1010</td>
+                                    <td>Musteren</td>
+                                    <td>041 414 32 10</td>
+                                    <td>max@muster.ch</td>
+                                    <td>26.05.1986</td>
+                                    <td><input type="checkbox" name="newsletter" checked></td>
+                                    <td><span class="glyphicon glyphicon-cog" aria-hidden="true"></span>
+                                        <span class="glyphicon glyphicon-remove" aria-hidden="true"></span></td>
+                                </tr><tr role="row" class="even">
+                                    <td class="sorting_1">Lea</td>
+                                    <td>Mustermann</td>
+                                    <td>Musterstrasse 16</td>
+                                    <td>1010</td>
+                                    <td>Musteren</td>
+                                    <td>041 414 43 21</td>
+                                    <td>lea@muster.ch</td>
+                                    <td>06.09.1981</td>
+                                    <td><input type="checkbox" name="newsletter" ></td>
+                                    <td><span class="glyphicon glyphicon-cog" aria-hidden="true"></span>
+                                        <span class="glyphicon glyphicon-remove" aria-hidden="true"></span></td>
+                                </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-5">
+                            <div class="dataTables_info" id="data-table_info" role="status" aria-live="polite">1 bis 2 von 2 Eintr�gen</div>
+                        </div><div class="col-sm-7">
+                        <div class="dataTables_paginate paging_simple_numbers" id="data-table_paginate">
+                            <ul class="pagination">
+                                <li class="paginate_button previous disabled" id="data-table_previous">
+                                    <a href="#" aria-controls="data-table" data-dt-idx="0" tabindex="0">Zur�ck</a>
+                                </li>
+                                <li class="paginate_button active">
+                                    <a href="#" aria-controls="data-table" data-dt-idx="1" tabindex="0">1</a>
+                                </li>
+                                <li class="paginate_button next disabled" id="data-table_next">
+                                    <a href="#" aria-controls="data-table" data-dt-idx="2" tabindex="0">N�chste</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            </div>
+
+            <button id="articles_create" type="button" onclick="loadItem('update_article','content','-1');" class="btn btn-success">Neuer Artikel</button>
+            <button id="articles_import" type="button" onclick="loadItem('import_article_overview','content','-1');" class="btn btn-success">Excel-Tabelle</button>
+
+
+        <script type="text/javascript">
+            $(document).ready(function() {
+
+                $('#data-table').DataTable({
+                    "language": {
+                        "sEmptyTable":      "Keine Daten in der Tabelle vorhanden",
+                        "sInfo":            "_START_ bis _END_ von _TOTAL_ Eintr�gen",
+                        "sInfoEmpty":       "0 bis 0 von 0 Eintr�gen",
+                        "sInfoFiltered":    "(gefiltert von _MAX_ Eintr�gen)",
+                        "sInfoPostFix":     "",
+                        "sInfoThousands":   ".",
+                        "sLengthMenu":      "_MENU_ Eintr�ge anzeigen",
+                        "sLoadingRecords":  "Wird geladen...",
+                        "sProcessing":      "Bitte warten...",
+                        "sSearch":          "Suchen",
+                        "sZeroRecords":     "Keine Eintr�ge vorhanden.",
+                        "oLanguage": {
+                            "sProcessing": "loading data..."
+                        },
+                        "oPaginate": {
+                            "sFirst":       "Erste",
+                            "sPrevious":    "Zur�ck",
+                            "sNext":        "N�chste",
+                            "sLast":        "Letzte"
+                        },
+                        "oAria": {
+                            "sSortAscending":  ": aktivieren, um Spalte aufsteigend zu sortieren",
+                            "sSortDescending": ": aktivieren, um Spalte absteigend zu sortieren"
+                        }
+                    },
+                    "bLengthChange": false,
+                    "pageLength": 10,
+                    "aoColumnDefs": [
+                        { "bSortable": false, "aTargets": [ 3, 4, 5 ] } ]
+                });
+            } );
+        </script>
+    </div>
