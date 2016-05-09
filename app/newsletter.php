9ade760e9bdcd9998d1a8819ed9c442902c7a5a5
Patrick Althaus <patrick.althaus@students.fhnw.ch>
2016-05-09 15:43:30 +0200

    Changed Wysiwyg

--- app/newsletter.php
+++ app/newsletter.php
@@ -5,22 +5,37 @@
  * Date: 09.05.2016
  * Time: 13:10
  */
+session_start();
+include("../api/dbconnect.php");
 ?>
+
+<!-- include summernote css/js-->
+<link href="../plugins/dist/summernote.css" rel="stylesheet">
+<script src="../plugins/dist/summernote.js"></script>
+
 <link rel="stylesheet" type="text/css" href="../plugins/datetimepicker-master/jquery.datetimepicker.css"/>
-<script src="../plugins/datetimepicker-master/jquery.js"></script>
 <script src="../plugins/datetimepicker-master/build/jquery.datetimepicker.full.min.js"></script>
 <table>
-    <td style="width: 500px;">
+    <td style="width: 600px;">
         <form method="post"  role="form" enctype="multipart/form-data" name="newsletter_queue">
             <h1>Newsletter versenden</h1>
             <div class="form-group" class="col-sm-7">
                 <label class="col-sm-12 control-label">Zu versenden um</label>
                 <div class="col-sm-12">
-                    <input id="datetimepicker" type="text">
+                    <input style="margin-bottom: 15px;" id="datetimepicker" type="text">
                 </div>
                 <label class="col-sm-12 control-label">Betreff</label>
                 <div class="col-sm-12">
-                    <input type="text" class="form-control" name="title" id="title">
+                    <input style="margin-bottom: 15px;" type="text" class="form-control" name="title" id="title">
+                </div>
+                <label class="col-sm-12 control-label">Inhalt</label>
+                <div class="col-sm-12">
+                    <div id="summernote"><p>Hello Summernote</p></div>
+                    <script>
+                        $(document).ready(function() {
+                            $('#summernote').summernote();
+                        });
+                    </script>
                 </div>
                 <br><button type="button" onclick="sendNewsletter();" style="margin-left: 15px;" class="btn btn-primary">Speichern</button>
             </div>
