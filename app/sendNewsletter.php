af739966412bfaadb7cb8dd18ca0f73446302c83
Patrick Althaus <patrick.althaus@students.fhnw.ch>
2016-05-10 13:32:27 +0200

    Implemented Newsletter logic

--- app/sendNewsletter.php
+++ app/sendNewsletter.php
@@ -15,9 +15,28 @@ if ($mysqli->connect_error) {
     die("Connection failed: " . $mysqli->connect_error);
 }
 //Get Template html
-$query = "SELECT template_text FROM newsletter_template WHERE template_code='Webshop Newsletter Template'";
+$query = "SELECT template_id, template_text FROM newsletter_template WHERE template_code='Webshop Template'";
 $result = $mysqli->query($query);
 $row = mysqli_fetch_assoc($result);
 
+$templateid = $row["template_id"];
+$template = $row["template_text"];
+//$timeelem = explode(' ',$_POST["datetime"]);
+$timeelem = explode(' ','2016 05 10 13 15 10');
+$timetosend = new DateTime();
+$timetosend->setTimezone(new DateTimeZone('Europe/Berlin'));
+$timetosend->setDate($timeelem[0],$timeelem[1],$timeelem[2]);
+$timetosend->setTime($timeelem[3],$timeelem[4],$timeelem[5]);
+$timetosend->setTimezone(new DateTimeZone('UTC'));
+$time = $timetosend->format('Y-m-d H:i:s');
+//$title = $_POST["title"];
+//$content = $_POST["content"];
+$title = 'adad';
+$content = 'adadadaddadadada';
+$conreplace = explode('h1>', $template);
+$html = rtrim($conreplace[0], '<')."<h1>".$title."</h1>\n".$content."\n".$conreplace[2];
 
+$insert = "INSERT INTO `newsletter_queue`(`queue_id`, `template_id`, `newsletter_type`, `newsletter_text`, `newsletter_styles`, `newsletter_subject`, `newsletter_sender_name`, `newsletter_sender_email`, `queue_status`, `queue_start_at`, `queue_finish_at`) VALUES (NULL,".$templateid.",NULL,'".$html."',NULL,'".$title."','Test','noreply@fhnw.ch','0','".$time."',NULL)";
+$mysqli->query($insert);
+echo $mysqli->error;
 $mysqli->close();
